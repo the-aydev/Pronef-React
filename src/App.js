4e03f0117aba2b5569f95ecce3dac8f4e2b2e927
Ayomide <ayomidedans@gmail.com>
2022-04-15 12:47:41 +0100

    deploy

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
+import { Download, Features, SectionWrapper } from './components';
+import assets from './assets';
+import styles from './styles/Global';
+
 const App = () => {
   return (
-    <div className="text-3xl font-bold underline">
-      <h1>React Native NFT ShowCase</h1>
-    </div>
+    <>
+      <SectionWrapper
+        title="Your own store of Nifty NFTs. Start Selling and Growing"
+        description="Buy, store, collect NFTs, exchange & earn crypto. Join 2+ million people using ProNef Marketplace."
+        showBtn
+        mockupImg={assets.homeHero}
+        banner="banner"
+      />
+      <SectionWrapper
+        title="Smart User Interface Marketplace"
+        description="Experience a buttery UI of ProNef NFT Marketplace."
+        mockupImg={assets.homeCards}
+        reverse
+      />
+      <Features />
+      <SectionWrapper
+        title="Deployment"
+        description="ProNef is build Expo which runs natively on all users devices. You can easily get your app into peoples hands"
+        mockupImg={assets.feature}
+        reverse
+      />
+      <SectionWrapper
+        title="Creative way to showcase the store"
+        description="The app contains two screens. The first screen lists all NFTs while the second screen shows the details of each specific."
+        mockupImg={assets.mockup}
+        banner="banner02"
+      />
+      <Download />
+      <div className="px-4 py-2 justify-center items-center bg-primary flex-col text-center banner04">
+        <p className={`${styles.pText} ${styles.whiteText}`}>Made with love by {" "}
+          <span className='bold'>the-Aydev</span></p>
+      </div>
+    </>
   );
 };
 
